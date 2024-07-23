fb18e46390af07cbac06906409833b1c84245327
2duckchun <kkts9308@gmail.com>
2024-07-23 23:55:46 +0900

    refact: Refactoring Header

--- src/widgets/layouts/header/Header.tsx
+++ src/widgets/layouts/header/Header.tsx
@@ -3,7 +3,6 @@
 import { FunctionComponent, useState } from 'react'
 import { Menu } from 'lucide-react'
 import { cn } from '@/lib/utils'
-import { NavigationMenuDemo } from './HeaderMenu'
 
 interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -11,59 +10,54 @@ export const Header: FunctionComponent<HeaderProps> = ({
   className,
   ...props
 }) => {
-  const [isOpen, setIsOpen] = useState(false)
-
   return (
     <header
       className={cn('sticky bg-white z-50 top-0 shadow-md', className)}
       {...props}
     >
       <DesktopHeader />
-      <div>
-        <section className="flex h-header w-full items-center justify-between px-6 md:hidden">
-          <h1 className="text-xl font-bold">토종스크립트</h1>
-          <Menu
-            className="cursor-pointer"
-            onClick={() => {
-              setIsOpen(!isOpen)
-            }}
-          />
-        </section>
-        {isOpen && (
-          <div className="bg-white p-3">
-            <div className="w-full rounded-lg bg-slate-400 px-3 pb-3 text-center">
-              <p className="py-2">커리큘럼</p>
-              <ul className="flex flex-col gap-1 rounded-lg bg-white">
-                <li className="m-auto flex h-10 w-[95%] cursor-pointer items-center justify-center hover:bg-slate-50">
-                  초급
-                </li>
-                <li className="m-auto flex h-10 w-[95%] cursor-pointer items-center justify-center hover:bg-slate-50">
-                  중급
-                </li>
-                <li className="m-auto flex h-10 w-[95%] cursor-pointer items-center justify-center hover:bg-slate-50">
-                  고급
-                </li>
-              </ul>
-            </div>
-            <div>아카이브</div>
-            <div>MDN</div>
-          </div>
-        )}
-      </div>
+      <MobileHeader />
     </header>
+  )
+}
+
+const MobileHeader = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div className="md:hidden">
+      <section className="flex h-header w-full items-center justify-between px-6 md:hidden">
+        <HeadingLogo />
+        <Menu
+          className="cursor-pointer"
+          onClick={() => {
+            setIsOpen(!isOpen)
+          }}
+        />
+      </section>
+      {isOpen && <TopNavigationMenu className="flex-col gap-3 pb-5" />}
+    </div>
   )
 }
 
 const DesktopHeader = () => {
   return (
     <section className="m-auto hidden h-header w-full items-center justify-between px-6 md:flex xl:w-[1280px]">
-      <div className="w-1/3">
-        <h1 className="text-2xl font-bold">토종스크립트</h1>
-      </div>
-      <div className="flex w-1/3 justify-center">
-        <NavigationMenuDemo />
-      </div>
-      <div className="flex w-1/3 justify-end">로그인</div>
+      <HeadingLogo />
+      <TopNavigationMenu />
     </section>
+  )
+}
+
+const HeadingLogo = () => {
+  return <h1 className="text-xl font-bold xl:text-2xl">2DC TECH</h1>
+}
+
+const TopNavigationMenu = ({ className }: { className?: string }) => {
+  return (
+    <ul className={cn('flex min-w-[500px] justify-evenly group:', className)}>
+      <li className="w-full text-center">POST</li>
+      <li className="w-full text-center">ABOUT ME</li>
+      <li className="w-full text-center">github</li>
+    </ul>
   )
 }
--- src/widgets/layouts/header/HeaderMenu.tsx
+++ src/widgets/layouts/header/HeaderMenu.tsx
@@ -1,64 +1,11 @@
 'use client'
 
 import * as React from 'react'
-import Link from 'next/link'
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle
-} from '@/components/ui/navigation-menu'
+import { NavigationMenuLink } from '@/components/ui/navigation-menu'
 import { cn } from '@/lib/utils'
 
 export function NavigationMenuDemo() {
-  return (
-    <NavigationMenu>
-      <NavigationMenuList className="gap-[40px]">
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>커리큘럼</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-6 md:w-[300px] lg:w-[400px]">
-              <ListItem href="/docs" title="입문">
-                자바스크립트 문법에 대해 간단히 배워봅시다.
-              </ListItem>
-              <ListItem href="/docs/installation" title="중급">
-                자바스크립트의 동작원리에 대해서 파헤쳐봅시다.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="고급">
-                비동기와 HTTP 프로토콜에 익숙해져 봅시다.
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>아카이브</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px]">
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              MDN
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-  )
+  return <div>dd</div>
 }
 
 const ListItem = React.forwardRef<
