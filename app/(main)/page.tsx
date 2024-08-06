c7c8fe4954ba95acbf07f076698d2d49d9dd3c6d
2duckchun <kkts9308@gmail.com>
2024-08-06 23:59:16 +0900

    test: 폴더 내 모든 파일 읽어들이기

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,19 @@
+import path from 'path'
+import { sync } from 'glob'
+import { Test } from '@/components/test'
 import { customCompileMdx } from '@/mdx/custom-compile-mdx'
 import { MarkDownModule } from '@/modules/mark-down-module'
+
+const CONTENT_PATH = path.join(process.cwd(), 'content', 'post')
+
+const getAllPosts = () => {
+  const postPaths: string[] = sync(`${CONTENT_PATH}/**/*.md`)
+  return postPaths.map((path) => {
+    return {
+      slug: path
+    }
+  })
+}
 
 export default async function Home() {
   const markdown = new MarkDownModule(['coding-test', 'baekjoon'])
@@ -8,10 +22,16 @@ export default async function Home() {
     sourse: data
   })
 
+  const postPath = getAllPosts()
+  console.log(postPath)
+  console.log(content)
+  console.log(frontmatter)
+
   return (
     <main className="flex min-h-screen flex-col items-center gap-5 p-2 md:p-24">
       <div className="w-full">
-        <div className="prose m-auto">{content}</div>
+        <Test>{content}</Test>
+        {/* <div className="prose m-auto">{content}</div> */}
       </div>
     </main>
   )
