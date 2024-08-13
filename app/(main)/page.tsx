ad35041b357669a0210f960c33b29f8b6d0f6ad7
2DC <kkts9308@gmail.com>
2024-08-13 23:35:42 +0900

    test: list rendering of markdown matters

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -5,17 +5,18 @@ import { Test } from '@/components/test'
 import { customCompileMdx } from '@/mdx/custom-compile-mdx'
 import { MarkDownModule } from '@/modules/mark-down-module'
 import matter from 'gray-matter';
+import { MarkDownMatter } from '@/types/matter'
 
 const CONTENT_PATH = path.join(process.cwd(), 'content', 'post')
 
 
-const parsePost = (postPath: string) => {
+const parsePost = (postPath: string): MarkDownMatter | undefined => {
   try {
     const file = fs.readFileSync(postPath, {encoding:'utf-8'})
-    const {content, data} = matter(file)
-    const grayMatter = data
+    const {data} = matter(file)
+    const grayMatter = data as MarkDownMatter
 
-    return {...grayMatter, content }
+    return {...grayMatter }
   } catch (error) {
     console.log(error)
   }
@@ -24,7 +25,7 @@ const parsePost = (postPath: string) => {
 
 const getAllPosts = () => {
   const postPaths: string[] = sync(`${CONTENT_PATH}/**/*.md`)
-  return postPaths.reduce<any[]>((acc, cur) => {
+  return postPaths.reduce<MarkDownMatter[]>((acc, cur) => {
     const post = parsePost(cur)
     if (!post) return acc
     return [...acc, post]
@@ -34,10 +35,10 @@ const getAllPosts = () => {
 
 export default async function Home() {
   const markdown = new MarkDownModule(['coding-test', 'baekjoon'])
-  const data = (await markdown.getMarkdownFile('2908')) as string
-  const { content, frontmatter } = await customCompileMdx({
-    sourse: data
-  })
+  // const data = (await markdown.getMarkdownFile('2908')) as string
+  // const { content, frontmatter } = await customCompileMdx({
+  //   sourse: data
+  // })
 
   const postPath = getAllPosts()
   console.log(postPath)
@@ -46,7 +47,19 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-5 p-2 md:p-24">
       <div className="w-full">
-        <Test>{content}</Test>
+      {postPath.map((item, index) => {
+        return (
+          <div key={index}>
+            <p>{item.title}</p>
+            <p>{item.description}</p>
+            <p>{String(item.created_date)}</p>
+            <p>{item.thumbnail}</p>
+            <p>{item.tags}</p>
+            <p>{item.draft}</p>
+          </div>
+        )
+      })}
+        {/* <Test>{content}</Test> */}
         {/* <div className="prose m-auto">{content}</div> */}
       </div>
     </main>
