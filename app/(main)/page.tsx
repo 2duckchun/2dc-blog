71ac5450827539f8ee2e563e46878eb4e96acaf5
2duckchun <kkts9308@gmail.com>
2024-08-13 23:11:55 +0900

    test: read md files in content directory

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,18 +1,35 @@
 import path from 'path'
+import fs from 'fs'
 import { sync } from 'glob'
 import { Test } from '@/components/test'
 import { customCompileMdx } from '@/mdx/custom-compile-mdx'
 import { MarkDownModule } from '@/modules/mark-down-module'
+import matter from 'gray-matter';
 
 const CONTENT_PATH = path.join(process.cwd(), 'content', 'post')
 
+
+const parsePost = (postPath: string) => {
+  try {
+    const file = fs.readFileSync(postPath, {encoding:'utf-8'})
+    const {content, data} = matter(file)
+    const grayMatter = data
+
+    return {...grayMatter, content }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
 const getAllPosts = () => {
   const postPaths: string[] = sync(`${CONTENT_PATH}/**/*.md`)
-  return postPaths.map((path) => {
-    return {
-      slug: path
-    }
-  })
+  return postPaths.reduce<any[]>((acc, cur) => {
+    const post = parsePost(cur)
+    if (!post) return acc
+    return [...acc, post]
+  }, [])
+
 }
 
 export default async function Home() {
@@ -24,8 +41,7 @@ export default async function Home() {
 
   const postPath = getAllPosts()
   console.log(postPath)
-  console.log(content)
-  console.log(frontmatter)
+
 
   return (
     <main className="flex min-h-screen flex-col items-center gap-5 p-2 md:p-24">
