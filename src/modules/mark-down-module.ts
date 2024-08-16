c722cda296e3da652d8ef84505a9adfcc3547fd7
2DC <kkts9308@gmail.com>
2024-08-16 10:17:11 +0900

    feat: add parseMarkdownGrayMatter and getGrayMatterList at mark-down-module.ts

--- src/modules/mark-down-module.ts
+++ src/modules/mark-down-module.ts
@@ -1,5 +1,9 @@
-import fs from 'fs/promises'
+import fs from 'fs'
+import fsPromise from 'fs/promises'
 import path from 'path'
+import { sync } from 'glob'
+import matter from 'gray-matter'
+import { MarkDownMatter } from '@/types/matter'
 
 export class MarkDownModule {
   private directoryPath: string
@@ -12,11 +16,30 @@ export class MarkDownModule {
   }
 
   async getMarkdownFile(filename: string) {
-    const MDFiles = await fs.readFile(
+    const MDFiles = await fsPromise.readFile(
       path.join(this.directoryPath, `${filename}.md`),
       'utf-8'
     )
 
     return MDFiles
+  }
+
+  parseMarkdownGrayMatter(postPath: string): MarkDownMatter | undefined {
+    try {
+      const file = fs.readFileSync(postPath, { encoding: 'utf-8' })
+      const { data: grayMatter } = matter(file)
+      return { ...grayMatter } as MarkDownMatter
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  getGrayMatterList() {
+    const postPaths: string[] = sync(`${this.directoryPath}/**/*.md`)
+    return postPaths.reduce<MarkDownMatter[]>((acc, cur) => {
+      const post = this.parseMarkdownGrayMatter(cur)
+      if (!post) return acc
+      return [...acc, post]
+    }, [])
   }
 }
