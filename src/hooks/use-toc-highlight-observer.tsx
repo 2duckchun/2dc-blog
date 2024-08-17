4967b5086961207fed6fdd340d23dfd4857f3083
2DC <kkts9308@gmail.com>
2024-08-17 21:20:46 +0900

    feat: add use-toc-highlight-observer

---
+++ src/hooks/use-toc-highlight-observer.tsx
@@ -0,0 +1,28 @@
+import { RefObject, useEffect, useState } from 'react'
+
+const options = {
+  root: null,
+  threshold: 0
+}
+
+export const useTocHighlightObserver = (
+  contentRef: RefObject<HTMLDivElement>
+) => {
+  const [highlightText, setHighlightText] = useState<string>()
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setHighlightText(entry.target.textContent ?? '')
+        }
+      })
+    }, options)
+    if (contentRef.current) {
+      const nodelist = contentRef.current.querySelectorAll('h1, h2, h3')
+      nodelist.forEach((toc) => observer.observe(toc))
+    }
+    return () => observer.disconnect()
+  }, [contentRef])
+
+  return { highlightText }
+}
