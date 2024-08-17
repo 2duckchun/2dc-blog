6000b0d9730e0f2a7d9c6ed778dd1f279f4836b4
2DC <kkts9308@gmail.com>
2024-08-17 21:39:28 +0900

    feat: apply post-toc-remote-controller component to page

--- src/components/post/post-content.tsx
+++ src/components/post/post-content.tsx
@@ -1,19 +1,14 @@
 'use client'
 
 import { FunctionComponent, HTMLAttributes } from 'react'
-import Link from 'next/link'
 import { useDragElement } from '@/hooks/use-drag-element'
 import { useGetTocAllNodeList } from '@/hooks/use-get-toc-all-node-list'
 import { useTocHighlightObserver } from '@/hooks/use-toc-highlight-observer'
 import { cn } from '@/lib/utils'
+import { PostTocRemoteController } from './post-toc-remote-controller'
 
 interface PostContentProps extends HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode
-}
-
-const options = {
-  root: null,
-  threshold: 0
 }
 
 export const PostContent: FunctionComponent<PostContentProps> = ({
@@ -27,60 +22,11 @@ export const PostContent: FunctionComponent<PostContentProps> = ({
 
   return (
     <div ref={contentRef} className={cn('prose m-auto', className)} {...props}>
-      <div
+      <PostTocRemoteController
         ref={draggableRef}
-        id="draggable"
-        className="left-3/4 top-[100px] z-10 hidden bg-slate-400 lg:fixed lg:block"
-      >
-        <div id="draggable-header" className="z-20 cursor-move bg-blue-500 p-5">
-          Click here to move
-        </div>
-        <ul className="list-none">
-          {tocList.map((item, idx) => {
-            if (item.nodeName === 'H1')
-              return (
-                <Link href={`#${item.id}`} key={item.id}>
-                  <li
-                    className={cn(
-                      highlightText === item.title && 'text-red-500'
-                    )}
-                    key={item.title}
-                  >
-                    {item.title}
-                  </li>
-                </Link>
-              )
-            if (item.nodeName === 'H2')
-              return (
-                <Link href={`#${item.id}`} key={item.id}>
-                  <li
-                    className={cn(
-                      'pl-3',
-                      highlightText === item.title && 'text-red-500'
-                    )}
-                    key={item.title}
-                  >
-                    {item.title}
-                  </li>
-                </Link>
-              )
-            if (item.nodeName === 'H3')
-              return (
-                <Link href={`#${item.id}`} key={item.id}>
-                  <li
-                    className={cn(
-                      'pl-5',
-                      highlightText === item.title && 'text-red-500'
-                    )}
-                    key={item.title}
-                  >
-                    {item.title}
-                  </li>
-                </Link>
-              )
-          })}
-        </ul>
-      </div>
+        highlightText={highlightText}
+        tocList={tocList}
+      />
       {children}
     </div>
   )
