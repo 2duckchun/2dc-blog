5b5e42a666a02e30f5e01599c2a9ce665c2303fb
2DC <kkts9308@gmail.com>
2024-08-17 21:20:31 +0900

    feat: add use-get-toc-all-node-list

---
+++ src/hooks/use-get-toc-all-node-list.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from 'react'
+import { TocData } from '@/types/post'
+
+export const useGetTocAllNodeList = (selector: string) => {
+  const contentRef = useRef<HTMLDivElement>(null)
+  const [tocList, setTocList] = useState<TocData[]>([])
+  useEffect(() => {
+    if (contentRef.current) {
+      const nodelist = contentRef.current.querySelectorAll(selector)
+      const rawTocData: TocData[] = []
+      nodelist.forEach((list, idx) => {
+        rawTocData.push({
+          title: list.textContent ?? '',
+          id: list.id,
+          nodeName: list.nodeName as Extract<TocData, 'nodeName'>
+        })
+      })
+      setTocList([...rawTocData])
+    }
+  }, [selector])
+
+  return {
+    contentRef,
+    tocList
+  }
+}
