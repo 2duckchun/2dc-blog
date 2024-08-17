53ee2e9697733dd90a63bf5908e0a2c9d7269cc8
2DC <kkts9308@gmail.com>
2024-08-17 10:58:37 +0900

    feat: commit MD feature to main page and post page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,66 +1,18 @@
-import path from 'path'
-import fs from 'fs'
-import { sync } from 'glob'
-import { Test } from '@/components/test'
-import { customCompileMdx } from '@/mdx/custom-compile-mdx'
+import { PostCard } from '@/components/post-card'
 import { MarkDownModule } from '@/modules/mark-down-module'
-import matter from 'gray-matter';
-import { MarkDownMatter } from '@/types/matter'
 
-const CONTENT_PATH = path.join(process.cwd(), 'content', 'post')
-
-
-const parsePost = (postPath: string): MarkDownMatter | undefined => {
-  try {
-    const file = fs.readFileSync(postPath, {encoding:'utf-8'})
-    const {data} = matter(file)
-    const grayMatter = data as MarkDownMatter
-
-    return {...grayMatter }
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-
-const getAllPosts = () => {
-  const postPaths: string[] = sync(`${CONTENT_PATH}/**/*.md`)
-  return postPaths.reduce<MarkDownMatter[]>((acc, cur) => {
-    const post = parsePost(cur)
-    if (!post) return acc
-    return [...acc, post]
-  }, [])
-
-}
+const CONTENT_PATH_LIST = ['content', 'post']
 
 export default async function Home() {
-  const markdown = new MarkDownModule(['coding-test', 'baekjoon'])
-  // const data = (await markdown.getMarkdownFile('2908')) as string
-  // const { content, frontmatter } = await customCompileMdx({
-  //   sourse: data
-  // })
-
-  const postPath = getAllPosts()
-  console.log(postPath)
-
+  const markdown = new MarkDownModule(CONTENT_PATH_LIST)
+  const grayMatterList = markdown.getFrontMatterList()
 
   return (
     <main className="flex min-h-screen flex-col items-center gap-5 p-2 md:p-24">
-      <div className="w-full">
-      {postPath.map((item, index) => {
-        return (
-          <div key={index}>
-            <p>{item.title}</p>
-            <p>{item.description}</p>
-            <p>{String(item.created_date)}</p>
-            <p>{item.thumbnail}</p>
-            <p>{item.tags}</p>
-            <p>{item.draft}</p>
-          </div>
-        )
-      })}
-        {/* <Test>{content}</Test> */}
-        {/* <div className="prose m-auto">{content}</div> */}
+      <div className="m-auto flex w-full flex-col gap-3 md:flex-row md:justify-evenly">
+        {grayMatterList.map((item, index) => {
+          return <PostCard key={item.title} frontmatter={item} />
+        })}
       </div>
     </main>
   )
--- app/(main)/post/[...slug]/page.tsx
+++ app/(main)/post/[...slug]/page.tsx
@@ -1,16 +1,23 @@
-interface PageProps {}
+import { PostContent } from '@/components/post-content'
+import { customCompileMdx } from '@/mdx/custom-compile-mdx'
+import { MarkDownModule } from '@/modules/mark-down-module'
 
-export default async function PostPage({
-  params: { slug }
-}: {
+interface PostPageProps {
   params: { slug: string[] }
-}) {
+}
+const CONTENT_PATH_LIST = ['content', 'post']
+
+const PostPage = async ({ params: { slug } }: PostPageProps) => {
+  const markdownModule = new MarkDownModule(CONTENT_PATH_LIST)
+  const rawMarkDown = await markdownModule.getMarkdownFile(slug[0])
+  const { content } = await customCompileMdx({
+    sourse: rawMarkDown
+  })
   return (
-    <main>
-      {slug.map((el, idx) => {
-        return <div key={idx}>{el}</div>
-      })}
-      Page
-    </main>
+    <section className="p-4 px-8">
+      <PostContent>{content}</PostContent>
+    </section>
   )
 }
+
+export default PostPage
