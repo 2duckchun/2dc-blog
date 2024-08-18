875beda653cc8457f978e28f52edf550d78d4668
2DC <kkts9308@gmail.com>
2024-08-18 13:21:25 +0900

    feat: add parseFilePath function to post-card component

--- src/components/post/post-card.tsx
+++ src/components/post/post-card.tsx
@@ -17,6 +17,13 @@ interface PostCardProps extends HTMLAttributes<HTMLDivElement> {
   frontmatter: MarkDownMatter
 }
 
+const parseFilePath = (fileName: MarkDownMatter['fileName']) => {
+  if (Array.isArray(fileName)) {
+    return fileName.join('/')
+  }
+  return fileName
+}
+
 export const PostCard: FunctionComponent<PostCardProps> = ({
   className,
   frontmatter,
@@ -27,35 +34,37 @@ export const PostCard: FunctionComponent<PostCardProps> = ({
   )
 
   return (
-    <Link className="w-fit" href={`/post/${frontmatter.fileName}`}>
+    <Link className="w-full" href={`/${parseFilePath(frontmatter.fileName)}`}>
       <Card
         key={frontmatter.title}
         className={cn(
-          'w-full md:w-[350px] cursor-pointer transition hover:-translate-y-3',
+          'w-full p-3 flex cursor-pointer transition hover:outline hover:translate-x-4',
           className
         )}
       >
-        <CardHeader>
-          <CardTitle>{frontmatter.title}</CardTitle>
-          <CardDescription>{CREATED_TIME}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex w-full justify-center py-5">
-            <Image
-              src={frontmatter.thumbnail}
-              width={250}
-              height={200}
-              className="rounded-lg"
-              alt={frontmatter.title}
-            />
-          </div>
-          <div>{frontmatter.description}</div>
-          <div className="flex gap-1">
-            {frontmatter.tags.map((item, index) => {
-              return <Badge key={item}>{item}</Badge>
-            })}
-          </div>
-        </CardContent>
+        <div className="hidden sm:flex sm:min-w-[250px] sm:justify-center">
+          <Image
+            src={frontmatter.thumbnail}
+            width={250}
+            height={200}
+            className="rounded-lg"
+            alt={frontmatter.title}
+          />
+        </div>
+        <div>
+          <CardHeader>
+            <CardTitle>{frontmatter.title}</CardTitle>
+            <CardDescription>{CREATED_TIME}</CardDescription>
+          </CardHeader>
+          <CardContent className="w-full">
+            <div>{frontmatter.description}</div>
+            <div className="flex flex-wrap gap-1 pt-5">
+              {frontmatter.tags.map((item, index) => {
+                return <Badge key={item}>{item}</Badge>
+              })}
+            </div>
+          </CardContent>
+        </div>
       </Card>
     </Link>
   )
