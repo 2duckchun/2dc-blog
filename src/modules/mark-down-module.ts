d3f5723d5fdfe81d956be19d522e041c307fea8c
2DC <kkts9308@gmail.com>
2024-08-18 13:20:00 +0900

    feat: add newParseMarkdownFrontMatter, newGetFrontMatterList, newExtractFileName method to MarkDownModule

--- src/modules/mark-down-module.ts
+++ src/modules/mark-down-module.ts
@@ -9,10 +9,10 @@ export class MarkDownModule {
   private directoryPath: string
   constructor(directoryPath: string | string[]) {
     if (Array.isArray(directoryPath)) {
-      this.directoryPath = path.join(process.cwd(), ...directoryPath)
+      this.directoryPath = path.join(process.cwd(), 'content', ...directoryPath)
       return
     }
-    this.directoryPath = path.join(process.cwd(), directoryPath)
+    this.directoryPath = path.join(process.cwd(), 'content', directoryPath)
   }
 
   async getMarkdownFile(filename: string) {
@@ -35,6 +35,17 @@ export class MarkDownModule {
     }
   }
 
+  newParseMarkdownFrontMatter(postPath: string): MarkDownMatter | undefined {
+    try {
+      const [path1, path2] = this.newExtractFileName(postPath)
+      const file = fs.readFileSync(postPath, { encoding: 'utf-8' })
+      const { data: frontMatter } = matter(file)
+      return { ...frontMatter, fileName: [path1, path2] } as MarkDownMatter
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   getFrontMatterList() {
     const postPaths: string[] = sync(`${this.directoryPath}/**/*.md`)
     return postPaths.reduce<MarkDownMatter[]>((acc, cur) => {
@@ -44,8 +55,60 @@ export class MarkDownModule {
     }, [])
   }
 
+  newGetFrontMatterList() {
+    const postPaths: string[] = sync(`${this.directoryPath}/**/*.md`)
+    return postPaths.reduce<MarkDownMatter[]>((acc, cur) => {
+      const post = this.newParseMarkdownFrontMatter(cur)
+      if (!post) return acc
+      return [...acc, post]
+    }, [])
+  }
+
+  getTagsWithFrontMatterList() {
+    const postPaths: string[] = sync(`${this.directoryPath}/**/*.md`)
+    const tagMap = new Map()
+    const reducedData = postPaths.reduce<MarkDownMatter[]>((acc, cur) => {
+      const post = this.newParseMarkdownFrontMatter(cur)
+      if (!post) return acc
+      return [...acc, post]
+    }, [])
+    tagMap.set('all', {
+      count: reducedData.length,
+      frontMatter: [...reducedData]
+    })
+
+    reducedData.forEach((items) => {
+      items.tags.forEach((tag) => {
+        if (!tagMap.has(tag)) {
+          tagMap.set(tag, {
+            count: 1,
+            frontMatter: [{ ...items }]
+          })
+        } else {
+          tagMap.set(tag, {
+            count: tagMap.get(tag).count + 1,
+            frontMatter: [...tagMap.get(tag).frontMatter, { ...items }]
+          })
+        }
+      })
+    })
+    return tagMap
+  }
+
   extractFileName(path: string) {
     const match = path.match(/\/([^\/]+)\.md$/)
     return match ? match[1] : null
+  }
+
+  newExtractFileName(path: string) {
+    const regex = /\/([^/]+)\/([^/]+)\.md$/
+    const match = path.match(regex)
+    if (match) {
+      const part1 = match[1]
+      const part2 = match[2]
+      return [part1, part2]
+    } else {
+      throw new Error('파일 찾기에 실패했습니다.')
+    }
   }
 }
