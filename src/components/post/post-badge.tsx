41a5a4b1eda9d980fe9d50b40f149f47058cc143
2DC <kkts9308@gmail.com>
2024-08-24 15:26:21 +0900

    feat: add pushRouteWithNewQueryString for sorting tag

--- src/components/post/post-badge.tsx
+++ src/components/post/post-badge.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { FunctionComponent, HTMLAttributes } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import { Badge } from '../ui/badge'
 
@@ -15,8 +16,26 @@ export const PostBadge: FunctionComponent<PostBadgeProps> = ({
   count,
   ...props
 }): JSX.Element => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+
+  const pushRouteWithNewQueryString = (name: string, value: string) => {
+    const currentURLSearchParams = new URLSearchParams(
+      Array.from(searchParams.entries())
+    )
+    currentURLSearchParams.set(name, value)
+    const current = currentURLSearchParams.toString()
+    const query = current ? `?${current}` : ''
+    router.push(`${pathname}${query}`)
+  }
+
   return (
-    <Badge className={cn('cursor-pointer', className)} {...props}>
+    <Badge
+      className={cn('cursor-pointer', className)}
+      {...props}
+      onClick={() => pushRouteWithNewQueryString('tag', title)}
+    >
       <span className="text-base">{title}</span>
       {count !== undefined && <span className="ml-2">{count}</span>}
     </Badge>
