6b394ea20eec3648ea4f2e2e98b1ae7f7a3694ef
2DC <kkts9308@gmail.com>
2024-08-26 17:50:59 +0900

    feat: add introduce-container to main page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,16 @@
+import { IntroduceContainer } from '@/components/layouts/container/introduce-container'
 import { PostCard } from '@/components/post/post-card'
 import { MarkDownModule } from '@/modules/mark-down-module'
 
 export default async function Home() {
   const markdown = new MarkDownModule('')
-  const grayMatterList = markdown.newGetFrontMatterList()
+  const frontMatterList = markdown.newGetFrontMatterList()
 
   return (
-    <main className="m-auto flex min-h-screen max-w-screen-xl flex-col items-center gap-5 p-2 md:p-24">
+    <main className="m-auto flex min-h-screen max-w-screen-xl flex-col items-center p-2">
+      <IntroduceContainer />
       <div className="m-auto flex w-full flex-col gap-3">
-        {grayMatterList.map((item, index) => {
+        {frontMatterList.map((item, index) => {
           return <PostCard key={item.title} frontmatter={item} />
         })}
       </div>
