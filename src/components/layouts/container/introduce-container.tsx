3b27644cf513c6f94a777f5aaacdc8eab49cbb43
2DC <kkts9308@gmail.com>
2024-08-26 17:50:37 +0900

    feat: add introduce-container

---
+++ src/components/layouts/container/introduce-container.tsx
@@ -0,0 +1,35 @@
+import { FunctionComponent, HTMLAttributes } from 'react'
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+import { MainPoppingText } from '../main-popping-text'
+
+interface IntroduceContainerProps extends HTMLAttributes<HTMLDivElement> {}
+
+export const IntroduceContainer: FunctionComponent<IntroduceContainerProps> = ({
+  className,
+  ...props
+}): JSX.Element => {
+  return (
+    <section
+      className={cn(
+        'w-[90%] my-10 flex flex-col md:flex-row items-center gap-5',
+        className
+      )}
+      {...props}
+    >
+      <div className="size-[150px] shrink-0 overflow-hidden rounded-full">
+        <Image
+          src={'/images/profile.png'}
+          width={150}
+          height={150}
+          alt={'프로필 사진'}
+        />
+      </div>
+      <div>
+        <MainPoppingText className="my-5 flex w-full gap-2" />
+        <p>안녕하세요.</p>
+        <p>개발자 2duckchun의 블로그입니다.</p>
+      </div>
+    </section>
+  )
+}
