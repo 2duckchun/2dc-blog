59393c86491b051ffff58aceffeac660c5a16671
2DC <kkts9308@gmail.com>
2024-09-04 21:22:26 +0900

    feat: add debounced-input component

---
+++ src/components/ui/debounced-input.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { cn } from '@/lib/utils'
+import { Input } from './input'
+
+interface DebouncedInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string
+  value: string | number
+  onDebounceHandler: (value: string | number) => void
+  debounce?: number
+}
+
+export const DebouncedInput = ({
+  className,
+  value: initialValue,
+  onDebounceHandler,
+  debounce = 500,
+  ...props
+}: DebouncedInputProps): JSX.Element => {
+  const [value, setValue] = useState(initialValue)
+
+  useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onDebounceHandler(value)
+    }, debounce)
+
+    return () => clearTimeout(timeout)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value])
+
+  return (
+    <Input
+      className={cn(className)}
+      {...props}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  )
+}
