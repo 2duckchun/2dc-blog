9730705fcee793e84eaf756029c04a1b2e20b3b6
2DC <kkts9308@gmail.com>
2024-09-10 12:13:38 +0900

    feat: fix toc logic (show all viewport TOC list item using useRef with useState

--- src/hooks/use-toc-highlight-observer.tsx
+++ src/hooks/use-toc-highlight-observer.tsx
@@ -1,28 +1,36 @@
-import { RefObject, useEffect, useState } from 'react'
+import { RefObject, useEffect, useRef, useState } from 'react'
 
 const options = {
   root: null,
-  threshold: 0
+  threshold: 1.0
 }
 
 export const useTocHighlightObserver = (
   contentRef: RefObject<HTMLDivElement>
 ) => {
-  const [highlightText, setHighlightText] = useState<string>('')
+  const [trigger, setTrigger] = useState<string>('')
+  const higtlightTextObject = useRef<Record<string, any>>({})
+
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          setHighlightText(entry.target.textContent ?? '')
+          setTrigger(entry.target.textContent ?? '')
+        }
+        if (entry.isIntersecting) {
+          higtlightTextObject.current[entry.target.textContent as string] = true
+        } else if (!entry.isIntersecting) {
+          higtlightTextObject.current[entry.target.textContent as string] =
+            false
         }
       })
     }, options)
     if (contentRef.current) {
-      const nodelist = contentRef.current.querySelectorAll('h2, h3')
+      const nodelist = contentRef.current.querySelectorAll('h1, h2, h3')
       nodelist.forEach((toc) => observer.observe(toc))
     }
     return () => observer.disconnect()
   }, [contentRef])
 
-  return { highlightText }
+  return { trigger, higtlightTextObject }
 }
