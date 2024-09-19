8d62970b049ae8edbd84678b988971c76694b862
2DC <kkts9308@gmail.com>
2024-09-20 00:27:48 +0900

    SEO: add google search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ export const metadata: Metadata = {
   description: '2DC의 개인 블로그',
   verification: {
     other: {
-      'naver-site-verification': '5da2532700825226e569779beb872ba677c13472'
+      'naver-site-verification': '5da2532700825226e569779beb872ba677c13472',
+      'google-site-verification': 'b3fTGYsUtZnvAfwHJri3JPJC5h1Chn_vGLdfPuCAb9g'
     }
   }
 }
