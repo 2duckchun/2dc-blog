fb77dc2d0476e9ea418ec9c6bf1533bb4f898b61
2DC <kkts9308@gmail.com>
2024-09-30 23:46:56 +0900

    refact: separate reusable code as a function

--- app/(main)/post/[...slug]/page.tsx
+++ app/(main)/post/[...slug]/page.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from 'next'
 import { PostContent } from '@/components/post/post-content'
 import { customCompileMdx } from '@/mdx/custom-compile-mdx'
 import { MarkDownModule } from '@/modules/mark-down-module'
@@ -7,19 +6,22 @@ interface PostPageProps {
   params: { slug: string[] }
 }
 
-export const metadata: Metadata = {
-  title: `POSTS`,
-  description: '그간 업로드했던 포스트를 보실 수 있습니다.'
+const getPostData = async (slug: string) => {
+  const markdownModule = new MarkDownModule(CONTENT_PATH_LIST)
+  const rawMarkDown = await markdownModule.getMarkdownFile(slug)
+  const { content, frontmatter } = await customCompileMdx({
+    sourse: rawMarkDown
+  })
+  return { content, frontmatter }
 }
+
+export async function generateMetadata() {}
 
 const CONTENT_PATH_LIST = ['post']
 
 const PostPage = async ({ params: { slug } }: PostPageProps) => {
-  const markdownModule = new MarkDownModule(CONTENT_PATH_LIST)
-  const rawMarkDown = await markdownModule.getMarkdownFile(slug[0])
-  const { content } = await customCompileMdx({
-    sourse: rawMarkDown
-  })
+  const { content } = await getPostData(slug[0])
+
   return (
     <main className="min-h-screen p-4 px-8">
       <PostContent>{content}</PostContent>
