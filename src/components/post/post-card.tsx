89f2bcae9e8646e738719b78630d64cbe8763256
2DC <kkts9308@gmail.com>
2025-01-12 23:49:09 +0900

    refactor: post-card 이미지 간격 조절

--- src/components/post/post-card.tsx
+++ src/components/post/post-card.tsx
@@ -35,23 +35,26 @@ export const PostCard: FunctionComponent<PostCardProps> = ({
           className
         )}
       >
-        <div className="hidden sm:flex sm:min-w-[250px] sm:justify-center">
-          <Image
-            src={frontmatter.thumbnail}
-            width={250}
-            height={100}
-            className="rounded-lg"
-            alt={frontmatter.title}
-          />
+        <div className="hidden items-center sm:flex sm:justify-center">
+          <div className="relative size-[150px]">
+            <Image
+              src={frontmatter.thumbnail}
+              fill
+              unoptimized
+              className="rounded-lg"
+              alt={frontmatter.title}
+            />
+          </div>
         </div>
+
         <div>
-          <CardHeader>
-            <CardTitle className="text-xl md:text-2xl">
+          <CardHeader className="py-2">
+            <CardTitle className="text-base md:text-xl">
               {frontmatter.title}
             </CardTitle>
             <CardDescription>{CREATED_TIME}</CardDescription>
           </CardHeader>
-          <CardContent className="w-full">
+          <CardContent className="w-full py-2">
             <div>{frontmatter.description}</div>
             <div role="tablist" className="flex flex-wrap gap-1 pt-3 md:pt-5">
               {frontmatter.tags.map((item) => {
