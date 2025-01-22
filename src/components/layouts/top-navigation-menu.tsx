3a5a96c793bf27b8f72fcdd992500f69b00c4ef4
2DC <kkts9308@gmail.com>
2025-01-22 21:44:03 +0900

    :recycle: problem solving 메뉴 제거

--- src/components/layouts/top-navigation-menu.tsx
+++ src/components/layouts/top-navigation-menu.tsx
@@ -16,10 +16,10 @@ const TOP_NAVIGATION_LIST = [
     title: 'POST',
     path: APP_PATH.POST()
   },
-  {
-    title: 'Problem Solving',
-    path: APP_PATH.PROBLEM_SOLVING()
-  },
+  // {
+  //   title: 'Problem Solving',
+  //   path: APP_PATH.PROBLEM_SOLVING()
+  // },
   {
     title: 'GITHUB',
     path: APP_PATH.GITHUB(),
